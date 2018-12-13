d68636164b84a2622b0ca706953929921e92f347
abejarano <abejarano@pinttosoft.com>
2018-12-12 21:35:08 -0400

    Customizing the project. root folder

--- kapture/main.ts
+++ kapture/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WsAdapter } from '@nestjs/websockets/adapters';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(3000);
+
+  /*
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  */
+
 }
 bootstrap();

5fac2be38a5edaacad1bbf8ba94da95137b19df8
abejarano <abejarano@pinttosoft.com>
2018-12-15 14:17:30 -0400

    finish model for wallet module

--- kapture/apps/wallets/models/wallets.schema.ts
+++ kapture/apps/wallets/models/wallets.schema.ts
@@ -7,15 +7,15 @@ const options: Joi.ValidationOptions = {abortEarly: true};
 joigoose = Joigoose(mongoose, options);
 
 /* wal_accounts */
-const joiAccountShema = Joi.object({
+const joiAccountSchema = Joi.object({
     account: Joi.string().guid().required(),
     seed: Joi.string().required(),
     created_at: Joi.date().default(Date.now()),
 });
-export const AccountShema = new mongoose.Schema(joigoose.convert(joiAccountShema));
+export const AccountSchema = new mongoose.Schema(joigoose.convert(joiAccountSchema));
 
 /* wal_settings_accounts */
-const joiAccountSettingsShema = Joi.object({
+const joiAccountSettingsSchema = Joi.object({
     account: Joi.string().required(),
     password_withdrawals: Joi.string().required(),
     created_at: Joi.date().default(Date.now()),
@@ -24,24 +24,22 @@ const joiAccountSettingsShema = Joi.object({
     notify_sms: Joi.boolean().default(false),
     two_factor: Joi.boolean().default(false),
 });
-export const AccountSettingShema = new mongoose.Schema(joigoose.convert(joiAccountSettingsShema));
+export const AccountSettingSchema = new mongoose.Schema(joigoose.convert(joiAccountSettingsSchema));
 
 /* wal_wallets */
-const joiWalletShema = Joi.object({
+const joiWalletSchema = Joi.object({
     account: Joi.string().required(),
     coin: Joi.string().max(3).required(),
     balance: Joi.number().default(0),
     child_change: Joi.number().integer().default(0),
     child: Joi.number().integer().default(0),
-    xpriv: Joi.string().base64().required(),
-    xpub: Joi.string().required(),
     address: Joi.string().required(),
     testnet: Joi.boolean().default(false),
 });
-export const WalletShema = new mongoose.Schema(joigoose.convert(joiWalletShema));
+export const WalletSchema = new mongoose.Schema(joigoose.convert(joiWalletSchema));
 
 /* wal_transactions */
-const joiTransactionShema = Joi.object({
+const joiTransactionSchema = Joi.object({
     address: Joi.string().required(),
     amount: Joi.number().positive().required(),
     tx: Joi.string().required(),
@@ -51,4 +49,25 @@ const joiTransactionShema = Joi.object({
     date_transaction: Joi.date().required(),
     wallet: Joi.string().required(),
 });
-export const TransactionShema = new mongoose.Schema(joigoose.convert(joiTransactionShema));
+export const TransactionSchema = new mongoose.Schema(joigoose.convert(joiTransactionSchema));
+
+/* wal_unspent_address */
+const  joiUnspentAddressSchema = Joi.object({
+    address: Joi.string().required(),
+    tx: Joi.string().required(),
+    script_pub: Joi.string().required(),
+    amount: Joi.number().positive().required(),
+    wallet: Joi.string().required(),
+});
+export const UnspentAddressSchema = new mongoose.Schema(joigoose.convert(joiUnspentAddressSchema));
+
+/* wal_withdrawal_request */
+const joiWithdrawalRequestSchema = Joi.object({
+    amount: Joi.number().positive().required(),
+    address: Joi.string().required(),
+    validation_code: Joi.string().required(),
+    status: Joi.string().max(1).default('P').required(),
+    created_at: Joi.date().default(Date.now()),
+    expires_at: Joi.date(),
+});
+export const WithdrawalRequestSchema = new mongoose.Schema(joigoose.convert(joiWithdrawalRequestSchema));
--- kapture/apps/wallets/providers/wallets.providers.ts
+++ kapture/apps/wallets/providers/wallets.providers.ts
@@ -1,25 +1,42 @@
 import {Connection} from 'mongoose';
-import { AccountShema, AccountSettingShema, WalletShema, TransactionShema } from '../models/wallets.schema';
+import {
+    AccountSchema,
+    AccountSettingSchema,
+    WalletSchema,
+    TransactionSchema,
+    UnspentAddressSchema,
+    WithdrawalRequestSchema,
+} from '../models/wallets.schema';
 
 export const walletsProviders = [
     {
         provide: 'AccountModel',
-        useFactory: (connection: Connection) => connection.model('wal_accounts', AccountShema),
+        useFactory: (connection: Connection) => connection.model('wal_accounts', AccountSchema),
         inject: ['DataBaseConnection'],
     },
     {
         provide: 'SettingAccountModel',
-        useFactory: (connection: Connection) => connection.model('wal_settings_accounts', AccountSettingShema),
+        useFactory: (connection: Connection) => connection.model('wal_settings_accounts', AccountSettingSchema),
         inject: ['DataBaseConnection'],
     },
     {
         provide: 'WalletModel',
-        useFactory: (connection: Connection) => connection.model('wal_wallets', WalletShema),
+        useFactory: (connection: Connection) => connection.model('wal_wallets', WalletSchema),
         inject: ['DataBaseConnection'],
     },
     {
         provide: 'TransactionModel',
-        useFactory: (connection: Connection) => connection.model('wal_transactions', TransactionShema),
+        useFactory: (connection: Connection) => connection.model('wal_transactions', TransactionSchema),
+        inject: ['DataBaseConnection'],
+    },
+    {
+        provide: 'UnspentAddressnModel',
+        useFactory: (connection: Connection) => connection.model('wal_unspent_address', UnspentAddressSchema),
+        inject: ['DataBaseConnection'],
+    },
+    {
+        provide: 'WithdrawalRequestModel',
+        useFactory: (connection: Connection) => connection.model('wal_unspent_address', WithdrawalRequestSchema),
         inject: ['DataBaseConnection'],
     },
 ];

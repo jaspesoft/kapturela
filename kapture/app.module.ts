c9d0ac357228f0f4b2605a6b5b38e3ecd1e3a9a3
abejarano <abejarano@pinttosoft.com>
2018-12-15 15:07:54 -0400

    update news modules

--- kapture/app.module.ts
+++ kapture/app.module.ts
@@ -5,11 +5,13 @@ import { WalletsModule } from './apps/wallets/wallets.module';
 import { Network } from './shared/provider/wallets/network';
 import { WalletsService } from './shared/services/wallets/wallets.service';
 import { SettingsModule } from './apps/settings/settings.module';
+import { CoinsModule } from './apps/coins/coins.module';
 
 @Module({
   imports: [
     WalletsModule,
     SettingsModule,
+    CoinsModule,
   ],
   controllers: [AppController],
   providers: [AppService, Network, WalletsService],

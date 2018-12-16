f05f5f2a6600dc29d35f5511d35f602872e7eece
abejarano <abejarano@pinttosoft.com>
2018-12-16 00:26:51 -0400

    fix module

--- kapture/app.module.ts
+++ kapture/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletsModule } from './apps/wallets/wallets.module';
 import { Network } from './shared/provider/wallets/network';
-import { WalletsService } from './shared/services/wallets/wallets.service';
 import { SettingsModule } from './apps/settings/settings.module';
 import { CoinsModule } from './apps/coins/coins.module';
 import { MailerModule } from '@nest-modules/mailer';
+import { Wallets } from './shared/provider/wallets/wallets';
 
 @Module({
   imports: [
@@ -16,6 +16,10 @@ import { MailerModule } from '@nest-modules/mailer';
     MailerModule.forRoot(),
   ],
   controllers: [AppController],
-  providers: [AppService, Network, WalletsService],
+  providers: [
+      AppService,
+      Network,
+      Wallets,
+  ],
 })
 export class AppModule { }
